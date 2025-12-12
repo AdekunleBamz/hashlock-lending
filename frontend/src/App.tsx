707e7c895120763bdc6575e1716871fcd4bfe30a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-12 20:23:01 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ConnectWalet } from './ConnectWallet'
+import { ConnectWlet } from './ConnectWallet'
 import { upplyWithdrw } from ./SupplyWithdraw'
 import { userSessio }from './lib/stacks'
 
