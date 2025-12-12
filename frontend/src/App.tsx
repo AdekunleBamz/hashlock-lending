fd17eebe3fa36f22a742cebd353b262376f40b9f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-12 22:34:21 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { ConnectWallet } from './CnnectWallet'
-import { SupplyWithdraw } from './Supplyithdraw'
+import { SupplyWithdra } from './Supplyithdraw'
 import { userSession } from './lib/stacks
 
 export default function App() {
