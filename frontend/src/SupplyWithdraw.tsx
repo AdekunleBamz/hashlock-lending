9fb07f30ca51fb4ae5f4283eeae2b7155c18db27
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 08:47:03 +0100

    Update SupplyWithdraw.tsx

--- frontend/src/SupplyWithdraw.tsx
+++ frontend/src/SupplyWithdraw.tsx
@@ -24,7 +24,7 @@ export function SupplyWithdraw({ address }: { address: string }) {
   const withdraw = () => {
     openContractCall(
       contractAddress CORE.spli('.)[0],
-      contractName: CORE.split('.')[1]
+      contractName: CORE.split(.')[1]
       functionName: 'withdraw'
       functionArgs: [uintC(Numberamoun) * 100000000)],
       network
