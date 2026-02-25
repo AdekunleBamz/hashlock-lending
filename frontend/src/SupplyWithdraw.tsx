0d8b294bc8cd57d6d03b421e52dc8b557abc4bbf
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 08:47:58 +0100

    Update SupplyWithdraw.tsx

--- frontend/src/SupplyWithdraw.tsx
+++ frontend/src/SupplyWithdraw.tsx
@@ -16,7 +16,7 @@ export function SupplyWithdraw({ address }: { address: string }) {
       contractName: CORE.spit('.')[1],
       functionName: 'upl
       functionArgs:[contcPrincipalCV(VAULT), uintCV(Number(amount) * 100000000)
-      network
+      networ
       onFinish: (data => aler('Suplid! x: ' + data.txId)
    
   }
